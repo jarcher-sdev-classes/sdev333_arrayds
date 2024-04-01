7bedc052b656c89de081d26630fa9942ab6075c7
Josh Archer <jarcher@greenriver.edu>
2024-03-31 20:44:07 -0700

    Adding starter files.

--- src/structure/ArraySet.java
+++ src/structure/ArraySet.java
@@ -1,21 +1,7 @@
 package structure;
 
-/**
- * This structure mirrors the mathematical set.
- * It does not allow duplicates...
- *
- * @author ???
- * @version ???
- */
 public class ArraySet
 {
-    private Object[] data;
-
-    public ArraySet()
-    {
-
-    }
-
 
 }
 
