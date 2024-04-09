3d71c1367e9fd89543a35a3c100ec4b550edd86a
Josh Archer <jarcher@greenriver.edu>
2024-04-09 13:58:23 -0700

    Removed typo from starter files.

--- src/tests/SetTest.java
+++ src/tests/SetTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import structure.SetAL;
 
 import java.util.*;
 
@@ -34,7 +33,7 @@ public class SetTest {
      */
     private ISet createSet() {
         //instantiate your class here!
-        return new SetAL();
+        return null;
     }
 
     /**
